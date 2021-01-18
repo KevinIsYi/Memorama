709fcc2c2382c4e26b30fe3473b59cc6e6758f5f
Kevin <rodriguezkevin95g@gmail.com>
2021-01-18 14:44:08 -0600

    Header will send to landing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
 import './header.css';
 
 export const Header = () => {
     return (
-        <header className="header text-center yellow-text bg-light-blue lobster">
-            Memorama
+        <header className="header text-center bg-light-blue">
+            <Link
+                to="/"
+                className="yellow-text lobster"
+            >
+                Memorama
+            </Link>
         </header>
     )
 }
